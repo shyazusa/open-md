24c70ff3fd20aed41c6c304be47f0361bb4d2728
takei <shy.azusa@gmail.com>
2016-03-03 13:49:46 +0900

    hot fix

--- app.rb
+++ app.rb
@@ -2,30 +2,29 @@ require 'sinatra'
 require 'redcarpet'
 
 get '/:directory/:file' do |d, f|
-  markdown("#{d}/#{f}", d.to_s)
+  markdown("#{d}/#{f}", "/#{d}/")
 end
 
 get '/:directory/' do |d|
-  markdown("#{d}/index", d.to_s)
+  markdown("#{d}/index", "/#{d}/")
 end
 
 get '/:file' do |f|
-  markdown(f.to_s, '')
+  markdown(f.to_s, '/')
 end
 
 get '/' do
-  markdown('index', '')
+  markdown('index', '/')
 end
 
 helpers do
   def markdown(filename, directory)
-    directory = '' unless File.exist?("md/#{directory}/index.md")
     filename = 'nofile' unless File.exist?("md/#{filename}.md")
     file = File.read("md/#{filename}.md", encoding: Encoding::UTF_8)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, fenced_code_blocks: true, highlight: true).render(file)
     @body = markdown
     @title = markdown.match(%r{<h1>(.*)</h1>})[1]
-    @back = "/#{directory}"
+    @back = "#{directory}"
     erb :index
   end
 end

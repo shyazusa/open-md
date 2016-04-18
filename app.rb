34870f813e3527eba504c2222181914a33d3e926
takei <shy.azusa@gmail.com>
2016-04-18 20:29:39 +0900

    (fix #33) Fix for #33

--- app.rb
+++ app.rb
@@ -2,36 +2,98 @@ require 'sinatra'
 require 'redcarpet'
 
 set :index, 'index'
+set :edit, 'edit'
 set :nofile, 'nofile'
+set :editlink, true
 set :backlink, true
 set :toplink, true
 
 get '/:directory/:file' do |d, f|
-  markdown("#{d}/#{f}", "/#{d}/")
+  if params['edit']
+    editMarkdown("#{d}/#{f}", "/#{d}/")
+  else
+    markdown("#{d}/#{f}", "/#{d}/")
+  end
 end
 
 get '/:directory/' do |d|
-  markdown("#{d}/#{settings.index}", "/#{d}/")
+  if params['edit']
+    editMarkdown("#{d}/#{settings.index}", "/#{d}/")
+  else
+    markdown("#{d}/#{settings.index}", "/#{d}/")
+  end
 end
 
 get '/:file' do |f|
-  markdown(f.to_s, '/')
+  if params['edit']
+    editMarkdown(f.to_s, '/')
+  else
+    markdown(f.to_s, '/')
+  end
 end
 
 get '/' do
-  markdown(settings.index, '/')
+  if params['edit']
+    editMarkdown(settings.index, '/')
+  else
+    markdown(settings.index, '/')
+  end
+end
+
+post '/:directory/:file' do |d, f|
+  saveMarkdown("#{d}/#{f}", "/#{d}/", params['body'])
+end
+
+post '/:directory/' do |d|
+  saveMarkdown("#{d}/#{settings.index}", "/#{d}/", params['body'])
+end
+
+post '/:file' do |f|
+  saveMarkdown(f.to_s, '/', params['body'])
+end
+
+post '/' do
+  saveMarkdown(settings.index, '/', params['body'])
 end
 
 helpers do
   def markdown(filename, directory)
+    @filename = filename
+    @directory = directory
     filename = settings.nofile unless File.exist?("md/#{filename}.md")
     file = File.read("md/#{filename}.md", encoding: Encoding::UTF_8)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, fenced_code_blocks: true, highlight: true).render(file)
-    @body = markdown
     @title = markdown.match(%r{<h1>(.*)</h1>})[1]
+    @body = markdown
     @back = "#{directory}"
+    @editlink = settings.editlink
     @backlink = settings.backlink
     @toplink = settings.toplink
     erb :index
+  end
+
+  def editMarkdown(filename, directory)
+    @filename = filename
+    @directory = directory
+    filename = settings.nofile unless File.exist?("md/#{filename}.md")
+    file = File.read("md/#{filename}.md", encoding: Encoding::UTF_8)
+    @title = file.match(%r{#\s(.*)})[1]
+    @body = file
+    @back = "#{directory}"
+    @editlink = settings.editlink
+    @backlink = settings.backlink
+    @toplink = settings.toplink
+    erb :edit
+  end
+
+  def saveMarkdown(filename, directory, body)
+    unless File.exist?("md/#{filename}.md")
+      File.open("md/#{filename}.md","w") do |file|
+        file.puts body
+      end
+    else
+      File.write("md/#{filename}.md", body)
+    end
+    markdown(filename, directory)
   end
 end
